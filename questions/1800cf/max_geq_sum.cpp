24d7c6bbebc7e2d8c62a1625ed536153c63db748
KaniCode1000 <cerebralpolymath@gmail.com>
2026-06-02 20:01:45 +0530

    Monotonic Stack and 1800 cf question

--- questions/1800cf/max_geq_sum.cpp
+++ questions/1800cf/max_geq_sum.cpp
@@ -2,38 +2,65 @@
 
 using namespace std;
 
-//D. Max GEQ Sum - Codeforces 1691
+//1691D. Max Geq Sum - Codeforces
 
 int main(){
-	int t;
-	cin >> t;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t; cin >> t;
 	while (t--){
-		int n;
-		cin >> n;
+		int n;cin >> n;
 		vector<long long> a(n);
 		for (int i = 0;i<n;++i){
 			cin >> a[i];
 		}
-		vector<pair<long long,long long>> dp(n);
-		dp[0] = {0,a[0]};
-		bool done = true;
+		
+		vector<long long> pref(n,0);
+		pref[0] = a[0];
 		for (int i = 1;i<n;++i){
-			if (a[i] > dp[i-1].second){
-				dp[i] = {max((long long) 0,dp[i-1].first+ 2*dp[i-1].second-a[i]),a[i]};
-			}	
-			else {
-				if (0 > dp[i-1].first + a[i]){
-					dp[i] = {0,a[i]};
-				}
-				else {
-					dp[i] = {dp[i-1].first + a[i],dp[i-1].second};
+			pref[i] = pref[i-1] + a[i];
+		}
+		auto rsum = [&](int L,int R){
+			if (L == 0)
+				return pref[R];
+			return pref[R] - pref[L-1];
+		};
+		vector<int> posies;
+		for (int i = 0;i<n;++i){
+			if (a[i] > 0) posies.push_back(i);
+		}
+		
+		bool done = true;
+		stack<int> stk,rstk;
+		for (int index: posies){
+			while(!stk.empty() && a[stk.top()] < a[index])
+				stk.pop();
+
+			if (!stk.empty()){
+				int l = stk.top(),r = index;
+				if (max(a[r],a[l]) < rsum(l,r)){
+					done = false; 
+					break;
 				}
 			}
-			if (dp[i].first > 0){
-				done = false;
-				break;
+			stk.push(index);
+		}
+
+		if (done){
+			for (int i = (int) posies.size()-1;i>=0;--i){
+				int index = posies[i];
+				while (!rstk.empty() && a[rstk.top()] < a[index])
+					rstk.pop();
+				if (!rstk.empty()){
+					int l = index,r = rstk.top();
+					if (max(a[r],a[l]) < rsum(l,r)){
+						done = false;
+						break;
+					}
+				}
+				rstk.push(index);
 			}
 		}
-		cout << ((done)?"YES\n":"NO\n");
+		cout << (done? "YES": "NO") << '\n';
 	}
 }
